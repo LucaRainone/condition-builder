bb9e94f1aad30fbbb188254e45bf85033b11d2f4
luca <luca.rainone@gmail.com>
2018-06-10 15:27:32 +0200

    ConditionBuilder as operator

--- src/ConditionBuilder.php
+++ src/ConditionBuilder.php
@@ -5,13 +5,15 @@ namespace rain1\ConditionBuilder;
 
 use rain1\ConditionBuilder\Operator\OperatorInterface;
 
-class ConditionBuilder
+class ConditionBuilder implements OperatorInterface
 {
 
     const MODE_AND = "AND";
     const MODE_OR = "OR";
 
     private $mode;
+
+    private $_isNot = false;
     /**
      * @var OperatorInterface[]
      */
@@ -32,17 +34,19 @@ class ConditionBuilder
         return $this;
     }
 
-    public function build()
+    public function build():string
     {
         $conditions = [];
         foreach ($this->elements as $element)
             if ($element->isConfigured())
                 $conditions[] = $element->build();
 
-        return "(".(implode(" {$this->mode} ", $conditions) ?: $this->getEmptyConditionValue()) .")";
+        $not = $this->_isNot? "!" : "";
+
+        return "$not(".(implode(" {$this->mode} ", $conditions) ?: $this->getEmptyConditionValue()) .")";
     }
 
-    public function values()
+    public function values():array
     {
         $values = [];
         foreach ($this->elements as $element)
@@ -55,5 +59,57 @@ class ConditionBuilder
     private function getEmptyConditionValue()
     {
         return $this->mode === self::MODE_AND ? "TRUE" : "FALSE";
+    }
+
+    private function _combine() {
+        $string = $this->build();
+        $values = $this->values();
+        $result = "";
+        $count = 0;
+        for($i = 0; $i < strlen($string); $i++) {
+            if($string[$i] === "?") {
+                $result .= json_encode($values[$count]);
+                $count++;
+            }else{
+                $result.=$string[$i];
+            }
+        }
+        return $result;
+    }
+
+    public function not()
+    {
+        $this->_isNot = !$this->_isNot;
+        return $this;
+    }
+
+    public function configure($conf)
+    {
+
+    }
+
+    public function isConfigured()
+    {
+        return true;
+    }
+
+    public function __toString()
+    {
+        return $this->build();
+    }
+
+    public function __invoke()
+    {
+        return $this->values();
+    }
+
+    public function __debugInfo()
+    {
+        return [
+            'condition' => $this->build(),
+            'values' => $this->values(),
+            'elements' => $this->elements,
+            'desired' => $this->_combine()
+        ];
     }
 }
